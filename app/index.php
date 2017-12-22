dab7b5e6c425e81740368c68e158af248b39e60e
cwc <icebowl1967@gmail.com>
2017-12-22 07:39:33 -0800

    aa

---
+++ app/index.php
@@ -0,0 +1,73 @@
+<html><head><title>icebowl.cc</title></head>
+<style>
+body{
+font-family: system-ui, sans-serif;
+}
+
+pre{
+background-color: #fdf6e3   ;
+color: #b58900;
+font-family: system-ui , sans-serif;
+border: solid 1px;
+border-radius: 20px;
+padding : 10px;
+}
+
+a{
+font-family: system-ui , sans-serif;
+	background-color:#cb4b16; ;
+	color: #fff;
+	font-size:20px;
+	text-decoration:none;
+	border: solid 1px;
+border-radius: 5px;
+padding : 2px;
+	
+}
+
+a:hover{
+	background-color: #dc322f; ;
+	color:  #fdf6e3; ;
+	text-decoration:none;
+	
+	
+}
+
+
+</style>
+<body>
+<center>
+<a href = "index.php?v=0" class = "menu" >GAME ON</a>&nbsp; 
+<a href = "index.php?v=1" class = "menu" >CHECK PS</a>&nbsp; 
+
+<hr />
+<?php
+if (isset($_GET['v'])) {
+	$thepost = $_GET['v']; // Default page
+	} else {
+		$thepost = -1;
+	}
+	
+if ($thepost == -1) {
+	$output = shell_exec('cal');
+	 echo "OUTPUT";
+	echo "<pre>$output</pre>";
+}
+if ($thepost == 0) {
+	$output = shell_exec('/home/cwc/http/app/hellotime.o');
+	 echo "OUTPUT";
+	echo "<pre>$output</pre>";
+}
+if ($thepost == 1) {
+	$output = shell_exec('ps');
+	 echo "OUTPUT";
+	echo "<pre>$output</pre>";
+}
+
+
+?>
+
+<hr />
+
+</body>
+</html>
